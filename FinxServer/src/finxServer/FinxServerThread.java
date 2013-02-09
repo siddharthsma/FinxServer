3bc2013366c3c82d4298cc2aac3a099d871f233b
Siddharth Ambegaonkar <siddharthsma@gmail.com>
2013-02-09 11:09:55 +0000

    Started Work on File sending
    Not sure where my folder has dissaperared !

--- FinxServer/src/finxServer/FinxServerThread.java
+++ FinxServer/src/finxServer/FinxServerThread.java
@@ -1,10 +1,13 @@
 package finxServer;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
@@ -15,9 +18,15 @@ public class FinxServerThread extends Thread {
 	private String finx_server_folder_path;
 	private InputStreamReader inReader;
 	private BufferedReader input;
-	private PrintStream output;
+	private FileOutputStream output;
+	private PrintStream outputProtocol;
 	private String stringMAC;
 	private String userIdentity;
+	private Socket finxSocket;
+	private InputStream in;
+	private DataInputStream clientData;
+	
+	
 	
 	// for the time being this is hardcoded - later it will be user specific
 	private String serverLogPath = "Assets/Serverlog.txt";
@@ -28,6 +37,7 @@ public class FinxServerThread extends Thread {
 	
 	public FinxServerThread(Socket finxSocket, String userIdentity) {
 		// setup - assuming the FinxFolder has already been created on the Server
+		this.finxSocket = finxSocket;
 		this.userIdentity = userIdentity;
 		setProtocolInputStream(finxSocket);
 		setProtocolOutputStream(finxSocket);
@@ -44,8 +54,29 @@ public class FinxServerThread extends Thread {
 		FinxServerController.replace_clients_map_Key(this, userIdentity, stringMAC);
 		testing();
 		sendLastPushTime();
+		setFileReceptionStream();
+		receiveFiles();
 		/* Next the client will need to check the Server log */
 		
+	}
+	
+	public void receiveFiles() {
+		int bytesRead; 
+		while (true) {
+			 try {
+				String fileName = clientData.readUTF();
+				setFileOutputStream(fileName);
+				long size = clientData.readLong();  
+				byte[] buffer = new byte[1024];
+				while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1)     
+		        {     
+		            output.write(buffer, 0, bytesRead);     
+		            size -= bytesRead;     
+		        }  
+			} catch (IOException e) {
+				e.printStackTrace();
+			}     
+		}
 	}
 	
 	public void sendLastPushTime() {
@@ -62,10 +93,28 @@ public class FinxServerThread extends Thread {
 					break;
 				}
 			}
-			output.println(stringedPushTime);
+			outputProtocol.println(stringedPushTime);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void setFileReceptionStream() {
+		try {
+			in = finxSocket.getInputStream();
+			clientData = new DataInputStream(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  
+           
+	}
+	
+	public void setFileOutputStream(String fileName) {
+		 try {
+			output = new FileOutputStream(fileName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
 	}
 	
 	public void setFileInputStream(File theFile, String path) {
@@ -89,7 +138,7 @@ public class FinxServerThread extends Thread {
 	
 	public void setProtocolOutputStream(Socket finxSocket) {
 		try {
-			output = new PrintStream(finxSocket.getOutputStream());
+			outputProtocol = new PrintStream(finxSocket.getOutputStream());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -104,7 +153,7 @@ public class FinxServerThread extends Thread {
 			e.printStackTrace();
 		}
 		// for the time being just authenticate any UUID
-		output.println("Authenticated");
+		outputProtocol.println("Authenticated");
 	}
 	
 	public void testing() {
